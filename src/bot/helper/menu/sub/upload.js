e74f8c47a147b526bea5a06e2a3bb8320c9bb5ed
Akmaljon571 <akmaljon079@gmail.com>
2023-09-17 17:49:31 +0300

    file uploads

--- src/bot/helper/menu/sub/upload.js
+++ src/bot/helper/menu/sub/upload.js
@@ -3,16 +3,41 @@ const _var = require("../../../../content/_var")
 const mainMenu = require("../../../../func/mainMenu")
 const Admin = require("../../../../model/admin")
 const SubCategory = require("../../../../model/subCategory")
+const axios = require('axios');
+const fs = require('fs')
 
 module.exports = async (msg) => {
     const chatId = msg.chat.id
-
+    
     if (msg.photo) {
-        const fileId = msg.photo.at(-1).file_id
+        const fileId = msg.photo.at(-1).file_id        
+        
+        bot.getFileLink(fileId).then((photoUrl) => {
+            const localFilePath = __dirname + `../../../../../upload/${fileId}.jpg`;
+        
+            axios({
+              method: 'get',
+              url: photoUrl,
+              responseType: 'stream',
+            })
+              .then((response) => {
+                const fileStream = fs.createWriteStream(localFilePath);
+                response.data.pipe(fileStream);
+        
+                fileStream.on('finish', () => {
+                  fileStream.close();
+                });
+              })
+              .catch((error) => {
+                console.error('Error downloading photo:', error);
+                bot.sendMessage(chatId, 'Failed to save the photo.');
+              });
+          });
+
+
         const admin = await Admin.findOne({ chatId })
         const findSub = await SubCategory.findOne({ _id: admin.action.split('--')[1] })
         findSub.img = fileId
-        console.log(findSub)
         await SubCategory.findByIdAndUpdate(findSub._id, findSub)
         bot.sendMessage(chatId, _var.save)
         await mainMenu(msg)
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ dotenv.config()
 const app = express()
 
 app.use(express.json())
+app.use(express.static('upload'));
 require('./bot')
 
 const main = async () => {
