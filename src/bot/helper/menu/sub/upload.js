49a83a419afb87fe09b96b8117783679b25d7f84
Akmaljon571 <akmaljon079@gmail.com>
2023-09-17 17:00:22 +0300

    finishcha

--- src/bot/helper/menu/sub/upload.js
+++ src/bot/helper/menu/sub/upload.js
@@ -8,15 +8,14 @@ module.exports = async (msg) => {
     const chatId = msg.chat.id
 
     if (msg.photo) {
-        bot.sendPhoto(chatId, 'AgACAgIAAxkDAAILPWUFZGvswxxJe0i1_0vke8WMbsEqAAIByzEbAzMwSHicP_YHmhwyAQADAgADbQADMAQ')
-        // const fileId = msg.photo.at(-1).file_id
-        // const admin = await Admin.findOne({ chatId })
-        // const findSub = await SubCategory.findOne({ _id: admin.action.split('--')[1] })
-        // findSub.img = fileId
-        // console.log(findSub)
-        // await SubCategory.findByIdAndUpdate(findSub._id, findSub)
-        // bot.sendMessage(chatId, _var.save)
-        // await mainMenu(msg)
+        const fileId = msg.photo.at(-1).file_id
+        const admin = await Admin.findOne({ chatId })
+        const findSub = await SubCategory.findOne({ _id: admin.action.split('--')[1] })
+        findSub.img = fileId
+        console.log(findSub)
+        await SubCategory.findByIdAndUpdate(findSub._id, findSub)
+        bot.sendMessage(chatId, _var.save)
+        await mainMenu(msg)
     } else {
         bot.sendMessage(chatId, _var.errUpload)
     }
